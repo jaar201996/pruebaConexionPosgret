0104aa377e9b8aa1e61536763ebdbc9f5465cdb1
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-02-26 23:15:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php 
-$dbconn = pg_connect(getenv(postgres://ihhieqvwqxhpov:3f34f2dde2ce1431743aa5e45760e95e8880eb552b86d9263b934acb8b8a92d2 @ ec2-3-213-192-58.compute-1.amazonaws.com : 5432 / d62v390lqlrs
-)); 
+    $host = "ec2-3-213-192-58.compute-1.amazonaws.com";
+    $port = "5432";
+    $dbname = "d62v390lqlrsn8";
+    $user = "ihhieqvwqxhpov";
+    $password = "3f34f2dde2ce1431743aa5e45760e95e8880eb552b86d9263b934acb8b8a92d2"; 
+    $connection_string = "host={$host} port={$port} dbname={$dbname} user= 
+              {$user} password={$password} ";
+    $dbconn = pg_connect($connection_string);   
   
 if($dbconn){
     echo "Connected established". pg_host($dbconn); 
@@ -9,4 +15,5 @@ if($dbconn){
 }
 
 ?>
+ 
  
