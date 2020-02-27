5da15274a575d124b5861024b19325504a055c93
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-02-26 23:40:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
-<?php 
-    $host = "ec2-3-213-192-58.compute-1.amazonaws.com";
-    $port = "5432";
-    $dbname = "d62v390lqlrsn8";
-    $user = "ihhieqvwqxhpov";
-    $password = "3f34f2dde2ce1431743aa5e45760e95e8880eb552b86d9263b934acb8b8a92d2"; 
-    $connection_string = "host={$host} port={$port} dbname={$dbname} user= 
-              {$user} password={$password} ";
-    $dbconn = pg_connect($connection_string);   
-  
-if($dbconn){
-    echo "Connected established". pg_host($dbconn); 
-}else{
-    echo "Somethng went wrong.";
-}
-
+<?php
+include("conexion.php");
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
