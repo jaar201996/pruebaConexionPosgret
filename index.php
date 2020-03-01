625b5a4955ce9fd335abea26a251e8d07558692d
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-03-01 10:57:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ include("conexion.php");
 	                                $nik='JK';
 					$date = date('Y-m-d H:i:s');
 					$horasalida = null;
-					$insert = pg_query($dbconn, "INSERT INTO asistencia(codigo, horaingreso, horasalida)
+					$insert = pg_query($dbconn, "INSERT INTO asistencias(codigo, horaingreso, horasalida)
 																VALUES('$nik','$date', '$horasalida')") or die("Fallo en insertar");
 					if($insert){
 								echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Bien hecho! Ya registro su ingreso.</div>';
